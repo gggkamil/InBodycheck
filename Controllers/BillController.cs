dd5da2906bb68d5ee1b6182b182cecc28f6ac5e9
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-11-23 18:31:10 +0100

    Add project files.

---
+++ Controllers/BillController.cs
@@ -0,0 +1,85 @@
+﻿using InBodycheck.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InBodycheck.Controllers
+{
+    public class BillController : Controller
+    {
+        private static IList<BillModel> bills = new List<BillModel>();
+        // GET: BillController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BillController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BillController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BillController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BillController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BillController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BillController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BillController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/BillModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InBodycheck.Models
+{
+    public class BillModel
+    {
+        public int ReceiptID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool  Done { get; set; }
+    }
+}
