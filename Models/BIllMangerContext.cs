7e37da182a6f2c980f413a2d7df1a7085910ce58
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-12-04 12:24:15 +0100

    db

---
+++ Models/BIllMangerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InBodycheck.Models
+{
+    public class BIllMangerContext : DbContext
+    {
+        public BIllMangerContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BillModel> Bills { get; set; }
+    }
+}
