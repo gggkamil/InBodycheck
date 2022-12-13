60403446d0a94619a451d72dde8b9e20523e8983
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-12-13 23:40:37 +0100

    lotsofchanges

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using InBodycheck.Models;
+using InBodycheck.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<BIllMangerContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BillManagerDatabase")));
+builder.Services.AddTransient<IBillRepository, BillRepository>();
 var app = builder.Build();
 
 
--- Repositories/BillRepository.cs
+++ Repositories/BillRepository.cs
@@ -4,29 +4,43 @@ namespace InBodycheck.Repositories
 {
     public class BillRepository : IBillRepository
     {
+        private readonly BIllMangerContext _context;
+        public BillRepository(BIllMangerContext context)
+        {
+            _context = context;
+        }
         public void Add(BillModel bill)
         {
-            throw new NotImplementedException();
+           _context.Bills.Add(bill);
+           _context.SaveChanges();
         }
 
         public void Delete(int billId)
         {
-            throw new NotImplementedException();
+           var result = _context.Bills.SingleOrDefault(x => x.ReceiptID== billId);
+            if (result != null)
+            {
+                _context.Bills.Remove(result);
+                _context.SaveChanges();
+            }
         }
 
         public BillModel Get(int billId)
-        {
-            throw new NotImplementedException();
-        }
+            => _context.Bills.SingleOrDefault(x => x.ReceiptID == billId);
 
         public IQueryable<BillModel> GetAllActive()
-        {
-            throw new NotImplementedException();
-        }
+            => _context.Bills.Where(x => !x.Done);
 
         public void Update(int billId, BillModel bill)
         {
-            throw new NotImplementedException();
+            var result = _context.Bills.SingleOrDefault(x=> x.ReceiptID == billId);
+            if (result != null)
+            {
+                result.Name= bill.Name;
+                result.Description= bill.Description;
+                result.Done = bill.Done;
+                _context.SaveChanges();
+            }
         }
     }
 }
